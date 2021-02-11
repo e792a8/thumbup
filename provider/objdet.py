146f991b8f0e4cc56cbbeab3937eaa3ec681aedb
zhou u74a8 <1571224208@qq.com>
2021-02-12 01:38:03 +0800

    incomplete objdet framework for master

--- provider/objdet.py
+++ provider/objdet.py
@@ -2,5 +2,5 @@ class ObjDetProvider:
 	def __init__(self, arg):
 		pass
 	def processFrame(self, img):
-		result = []
+		result = [("testing",666,(200,200,400,400))]
 		return result # [(class, score, (box)), ...]
