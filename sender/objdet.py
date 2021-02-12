629b82c79b176c9e101472fddf56e0df6cd5840b
zhou u74a8 <1571224208@qq.com>
2021-02-12 11:09:40 +0800

    server rebuild 1

--- sender/objdet.py
+++ sender/objdet.py
@@ -13,36 +13,18 @@ class ObjDetSender(BaseSender):
 
 	def processClient(self, client, addr):
 		result = self.recvPack(client, "!hhh")	#RECV fullscore w h
-		if result == False:
-			self.killClient(client)
-			return
 		self.fullscore = result[0]
 		self.resolution = (result[1],result[2])
-		result = self.sendPack(client, "!hhh", (self.fullscore,*self.resolution))	#SEND fullscore w h
-		if result == False:
-			self.killClient(client)
-			return
+		self.sendPack(client, "!hhh", (self.fullscore,*self.resolution))	#SEND fullscore w h
 		
 		while self.running:	#LOOP
 
 			result = self.recvPack(client, "!q")	#RECV reqtstp
-			if result == False:
-				self.killClient(client)
-				return
 			timestamp, frame = self.seekFrame(result[0])
 			num = len(frame)
-			result = self.sendPack(client, "!ql", (timestamp, num))	#SEND timestamp number
-			if result == False:
-				self.killClient(client)
-				return
+			self.sendPack(client, "!ql", (timestamp, num))	#SEND timestamp number
 
 			for i in frame:
 				name = i[0].encode("utf8")
-				result = self.sendPack(client, "!hhhhhh", (i[1], *i[2], len(name)))	#SEND score (box) len(name)
-				if result == False:
-					self.killClient(client)
-					return
-				result = self.sendRaw(client, name)	#SEND name
-				if result == False:
-					self.killClient(client)
-					return
+				self.sendPack(client, "!hhhhhh", (i[1], *i[2], len(name)))	#SEND score (box) len(name)
+				self.sendRaw(client, name)	#SEND name
